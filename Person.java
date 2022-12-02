7f8b819c35c5371d98b8d3a4091e747893e5e625
Kayskye <118865660+Kayskye@users.noreply.github.com>
2022-12-01 20:42:59 -0500

    Update Person.java

--- Person.java
+++ Person.java
@@ -6,7 +6,16 @@ public abstract class Person{
     protected String address;
     protected String email;
     protected String telNum;
-
+    
+     /**
+     * This method is used to initialise a person and is a parent class constructor for: Parent
+     * @param name Name of person
+     * @param dob Date of birth of parent
+     * @param sex Gender of person
+     * @param addr Address of person
+     * @param email Email of person
+     * @param telNum Telephone number of person
+     */
     public Person(String name, String dob, Sex sex, String addr, String email, String telNum){
         this.name=name;
         this.dob=dob;
@@ -16,21 +25,56 @@ public abstract class Person{
         this.telNum=telNum;
     }
 
+    /**
+     * 
+     * @return Name of person
+     */
     public abstract String getName();
-
+    
+    /**
+     * 
+     * @return Date of birth of person
+     */
     public abstract String getDOB();
 
+     /**
+     * 
+     * @return Gender of person
+     */
     public abstract Sex getSex();
 
+    /**
+     * 
+     * @return The address of person
+     */
     public abstract String getAddr();
 
+    /**
+     * @param newAddr Address of person
+     */
     public abstract void setAddr(String newAddr);
 
+    /**
+     * 
+     * @return Email of person
+     */
     public abstract String getEmail();
 
+    /**
+     * 
+     * @param new_Email Email of person
+     */
     public abstract void setEmail(String new_Email);
 
+    /**
+     * 
+     * @return Telephone number of person
+     */
     public abstract String getTelNum();
 
+    /**
+     * 
+     * @param Tel_num Telephone number of person
+     */
     public abstract void setTelNum(String Tel_num);
 }
